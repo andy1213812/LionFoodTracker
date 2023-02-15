530abe90f171f8adc59ce726f9d952e5a6e5f499
Yuan Chen <67477126+andy1213812@users.noreply.github.com>
2023-02-15 15:06:07 -0500

    Initial Commit

---
+++ LionFoodTracker/LionFoodTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LionFoodTrackerApp.swift
+//  LionFoodTracker
+//
+//  Created by yuan chen on 2/15/23.
+//
+
+import SwiftUI
+
+@main
+struct LionFoodTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
